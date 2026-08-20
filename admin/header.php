7a6034b2f6d0961e2eb5a20d07bc55eccb3102aa
Rahul Dhiman <rahul.dhiman.mohanlal@gmail.com>
2026-08-20 23:56:46 +0530

    updated

--- admin/header.php
+++ admin/header.php
@@ -393,36 +393,23 @@ $active_page = $active_pageInfo['active_page'] ?? null;
         <!-- Wrapper started -->
 
         <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+            <!-- Left navbar links -->
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <div class="nav-link">
-                        <i class="fas fa-th-large"></i>
-                    </div>
-                </li>
-                <li class="nav-item d-none d-sm-inline-block">
-                    <a href="./" class="nav-link">Home</a>
+                    <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
                 </li>
             </ul>
-            <form class="form-inline ml-3">
-                <div class="input-group input-group-sm">
-                    <input class="form-control form-control-navbar" type="search" placeholder="Search" name="search">
-                    <div class="input-group-append">
-                        <button class="btn btn-navbar" type="submit">
-                            <i class="fas fa-search"></i>
-                        </button>
-                    </div>
-                </div>
-            </form>
+
+            <!-- Right navbar links -->
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item dropdown">
-                    <a class="nav-link" href="#messages">
-                        <i class="far fa-comments"></i>
-                        <span class="badge badge-danger navbar-badge">2</span>
+                <li class="nav-item">
+                    <a class="nav-link" href="profile.php" title="Profile">
+                        <i class="fas fa-user-circle mr-1"></i> Profile
                     </a>
                 </li>
-                <li class="nav-item dropdown"><a class="nav-link" href="#notifications">
-                        <i class="far fa-bell"></i>
-                        <span class="badge badge-warning navbar-badge">5</span>
+                <li class="nav-item">
+                    <a class="nav-link" href="javascript:void(0);" onclick="logout()" title="Logout" style="color: #dc3545 !important;">
+                        <i class="fas fa-sign-out-alt mr-1"></i> Logout
                     </a>
                 </li>
             </ul>
